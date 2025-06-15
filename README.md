51fb541b5feee3b7ded68be20d88a1a724cd66a0
Prince Mukunzi <p.niyomugab@alustudent.com>
2025-06-15 10:51:35 +0200

    Efficient Error Handling; Detailed README file

--- README.md
+++ README.md
@@ -1,16 +1,26 @@
-# Submission Reminder (Shell Script)
+# Submission Reminder App (Shell Script)
 
-# How it works
+## Get Set up – Environment
+1. chmod +x create_environment.sh
+2. ./create_environment.sh
+3. Navigate to submission_reminder_<user_name>
+4. ./startup.sh 
+
+## Get set up – Copilot
+1. chmod +x copilot_shell_script.sh
+2. ./copilot_shell_script 
+
+## How it works – Environment
 
 - Prompts for user's name
 - Creates a folder: `submission_reminder_<user_name>`
 - Sets up necessary(provided) files under the folder for the program to work
-- When you run the reminder, it tells you who hasn’t submitted
+- When you run the app<startup.sh>, it tells you who hasn’t submitted <assignment> Default: "Shell navigation"
 
-## How to run
 
-1. chmod +x create_environment.sh
-2. ./create_environment.sh
-3. Navigate to submission_reminder_<user_name>
-4. ./startup.sh
+## How it works – Copilot
+1. Prompts user name–must be the same as user name used in Environment
+3. Prompts assignment to check
+4. Runs the reminder and tells you who hasn't submitted specified assignment
 
+## 
--- copilot_shell_script.sh
+++ copilot_shell_script.sh
@@ -1,16 +1,38 @@
 #!/bin/bash
 
-#Prompt the user for name to check
+#Prompt the user for name and assignment to check
 read -p "Enter name: " user_name
 
-#Getting variable sources
-source ./submission_reminder_$user_name/config/config.env
 
-#variable to store directory path
-path=./submission_reminder_$user_name
 
-read -p "Enter Assignment to check: " user_assignment
-perl -pi -e "s/$ASSIGNMENT/$user_assignment/" $path/config/config.env
+#Declared variables
+path="./submission_reminder_${user_name}"
+found=false
+possible_assignments=("Shell Navigation" "Git" "Shell Basics")
 
-#run the startup file
-$path/startup.sh
+#checks if entered user name is not null and is an actual directory
+if [[ -n $user_name && -d $path ]]; then
+    #Getting Environment config variables
+    source ./submission_reminder_${user_name}/config/config.env
+    read -p "Enter Assignment to check: " user_assignment
+    #check if entered assignment is in assignments
+    for assignment in "${possible_assignments[@]}"; do
+        if [ "${assignment}" == "${user_assignment}" ]; then
+            found=true
+            perl -pi -e "s/$ASSIGNMENT/$user_assignment/" $path/config/config.env
+            #run the startup file
+            $path/startup.sh
+            #close the loop if it assignment is found
+            break
+            exit 0
+        fi
+    done
+    #Error in case assignment is not found
+    if [ "$found" == false ]; then
+        echo "Error: Assignment not found; Please provide Valid Assignment">&2
+        exit 1
+    fi
+else
+echo "Error: Invalid Username">&2
+exit 1
+fi
